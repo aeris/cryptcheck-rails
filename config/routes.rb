d3c77b738a6e1289f552667c58ffc85b80986694
aeris <aeris@imirhil.fr>
2019-12-01 23:28:59 +0100

    Enable Sidekiq UI on development

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,44 @@
 Rails.application.routes.draw do
-	namespace :https, id: /[^\/]+/ do
-		get ':id/', action: :show
-		get ':id/refresh', action: :refresh, as: :refresh
-	end
+  namespace :https, id: /[^\/]+/ do
+    get ':id/', action: :show
+    get ':id/refresh', action: :refresh, as: :refresh
+  end
 
-	namespace :smtp, id: /[^\/]+/ do
-		get ':id/', action: :show
-		get ':id/refresh', action: :refresh, as: :refresh
-	end
+  namespace :smtp, id: /[^\/]+/ do
+    get ':id/', action: :show
+    get ':id/refresh', action: :refresh, as: :refresh
+  end
 
-	namespace :xmpp, id: /[^\/]+/ do
-		get ':id/', action: :show
-		get ':id/refresh', action: :refresh, as: :refresh
-	end
+  namespace :xmpp, id: /[^\/]+/ do
+    get ':id/', action: :show
+    get ':id/refresh', action: :refresh, as: :refresh
+  end
 
-	namespace :tls, id: /[^\/]+/ do
-		get '/', action: :index
-		get ':id/', action: :show
-		get ':id/refresh', action: :refresh, as: :refresh
-	end
+  namespace :tls, id: /[^\/]+/ do
+    get '/', action: :index
+    get ':id/', action: :show
+    get ':id/refresh', action: :refresh, as: :refresh
+  end
 
-	namespace :ssh, id: /[^\/]+/ do
-		get '/', action: :index
-		get ':id/', action: :show
-		get ':id/refresh', action: :refresh, as: :refresh
-	end
+  namespace :ssh, id: /[^\/]+/ do
+    get '/', action: :index
+    get ':id/', action: :show
+    get ':id/refresh', action: :refresh, as: :refresh
+  end
 
-	get 'help' => 'site#help'
-	get 'about' => 'site#about'
-	get 'ciphers' => 'site#ciphers'
-	get 'suite' => 'site#suite_index'
-	get 'suite/:id' => 'site#suite'
-	post 'suite' => 'site#suite'
-	root 'site#index'
-	post '/' => 'site#check'
+  get 'help' => 'site#help'
+  get 'about' => 'site#about'
+  get 'ciphers' => 'site#ciphers'
+  get 'suite' => 'site#suite_index'
+  get 'suite/:id' => 'site#suite'
+  post 'suite' => 'site#suite'
+  root 'site#index'
+  post '/' => 'site#check'
 
-	get 'sites' => 'site#sites'
+  get 'sites' => 'site#sites'
+
+  if Rails.env.development?
+    require 'sidekiq/web'
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
