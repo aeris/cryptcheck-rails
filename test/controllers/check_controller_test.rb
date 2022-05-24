ac4345856cb6a844bbf12d20a1c58fb0077d07f7
aeris <aeris@imirhil.fr>
2022-05-24 14:07:44 +0200

    Clean empty test

--- test/controllers/check_controller_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class CheckControllerTest < ActionController::TestCase
-end
--- test/controllers/ssh_controller_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class SshControllerTest < ActionController::TestCase
-end
